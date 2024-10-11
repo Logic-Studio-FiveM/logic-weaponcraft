e82b600e3abe72d9edbcb675eed22ddc78f560ac
Atoshi <95099733+atoshit@users.noreply.github.com>
2024-10-11 15:44:40 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Atoshi'
+description 'FiveM Weapon Crafting in a OX Menu'
+version '1.0.0'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'shared/configuration.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'es_extended'
+}
